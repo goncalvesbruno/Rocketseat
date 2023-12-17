e885f3a5fe3535fd0b6eb1a40c91aa9ba4a3e284
Bruno <bcgoncalves@inf.ufpel.edu.br>
2023-12-17 17:22:01 -0300

    Criação do banco de dados com Docker

--- Java/gestao-vagas/src/main/java/br/com/bruno/gestaovagas/modules/candidate/CandidateEntity.java
+++ Java/gestao-vagas/src/main/java/br/com/bruno/gestaovagas/modules/candidate/CandidateEntity.java
@@ -1,9 +1,15 @@
 package br.com.bruno.gestaovagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
@@ -13,10 +19,18 @@ import lombok.Data;
 *   Annotation @Pattern padroniza a entrada, dependência spring-boot-starter-validation
 *   Annotation @Email valida se é um e-mail válido
 *   Annotation @Length limita o tamanho da senha
+*   @Entity irá transformar o objeto em uma tabela para adicionar no DB. Parâmetro name é o nome da tabela. 
+*   Autimaticamente com o @Entity os atributos serão transformados em colunas
+*   @Column altera o nome da coluna no banco de dados caso necessário
+*   @Id irá informar que o atributo será o ID das entradas na tabela (precisa ser do jakarta)
+*   @GeneratedValue toda vez que for criado um objeto, será criado um ID único para a tabela
 */
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
 
@@ -31,4 +45,6 @@ public class CandidateEntity {
     private String description;
     private String curriculum;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
